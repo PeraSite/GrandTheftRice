78f620742e2e25e521660d4eb82dfb5e800d7220
PeraSite <pretocki3@gmail.com>
2022-08-25 11:13:18 +0900

    feat: 체력 회복 아이템 추가

---
+++ Assets/_GrandTheftRice/Characters/Scripts/Behaviour/HealOnCollide.cs
@@ -0,0 +1,32 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace GrandTheftRice {
+	public class HealOnCollide : MonoBehaviour {
+		[Title("설정")]
+		[SerializeField] private float _healAmount = 1;
+		[SerializeField] private bool _destroyOnCollide = true;
+		[SerializeField] private bool _useTrigger;
+
+		[SerializeField] private string _tagToHeal = "Player";
+
+		private void OnTriggerEnter2D(Collider2D col) {
+			if (!_useTrigger) return;
+			OnHit(col.gameObject);
+		}
+
+		private void OnCollisionEnter2D(Collision2D col) {
+			if (_useTrigger) return;
+			OnHit(col.gameObject);
+		}
+
+		private void OnHit(GameObject go) {
+			if (!go.TryGetComponent<Damageable>(out var damageable)) return;
+			if (!damageable.CompareTag(_tagToHeal)) return;
+
+			damageable.Heal(_healAmount);
+			if (_destroyOnCollide)
+				Destroy(gameObject);
+		}
+	}
+}
