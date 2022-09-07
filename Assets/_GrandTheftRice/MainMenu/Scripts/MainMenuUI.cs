ffec972d070484c0cd91c3d2735de6a3edbc3299
PeraSite <pretocki3@gmail.com>
2022-09-07 18:52:49 +0900

    feat: 메인 메뉴 추가

---
+++ Assets/_GrandTheftRice/MainMenu/Scripts/MainMenuUI.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using DG.Tweening;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace GrandTheftRice.MainMenu {
+	public class MainMenuUI : MonoBehaviour {
+		[SerializeField] private Vector3 _defaultCameraPosition;
+		[SerializeField] private Vector3 _settingCameraPosition;
+		[SerializeField] private Vector3 _rankingCameraPosition;
+
+
+		private Camera _cam;
+		[SerializeField] private float _animationDuration = 0.5f;
+
+		private void Awake() {
+			_cam = Camera.main;
+		}
+
+		[Button]
+		public void ShowSetting() => MoveCameraTo(_settingCameraPosition);
+
+		[Button]
+		public void ShowRanking() => MoveCameraTo(_rankingCameraPosition);
+
+		[Button]
+		public void ShowMainScreen() => MoveCameraTo(_defaultCameraPosition);
+
+		private void MoveCameraTo(Vector3 position) {
+			_cam.transform.DOMove(position, _animationDuration);
+		}
+	}
+}
