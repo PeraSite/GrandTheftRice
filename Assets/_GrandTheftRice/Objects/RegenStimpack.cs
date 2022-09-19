4b034a8d3575558fc9a94d9584d9c5725ec0f370
PeraSite <pretocki3@gmail.com>
2022-09-19 18:20:47 +0900

    feat: 일정 시간마다 재생되는 힐 아이템 추가

---
+++ Assets/_GrandTheftRice/Objects/RegenStimpack.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using MoreMountains.TopDownEngine;
+using UnityEngine;
+
+namespace Objects {
+	public class RegenStimpack : Stimpack {
+		[Header("Regenerate")]
+		[Tooltip("How much time it takes to regenerate after the pickup")]
+		[SerializeField] private float _regenTime;
+
+		private WaitForSecondsRealtime _awaiter;
+
+		private void Awake() {
+			_awaiter = new WaitForSecondsRealtime(_regenTime);
+		}
+
+		protected override void Pick(GameObject picker) {
+			base.Pick(picker);
+			StartCoroutine(Regenerate());
+		}
+
+		private IEnumerator Regenerate() {
+			yield return _awaiter;
+			Model.SetActive(true);
+			_collider2D.enabled = true;
+		}
+	}
+}
