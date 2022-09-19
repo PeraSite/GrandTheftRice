b7b8c7ae44183bd939b737b87e95a75ccc3a80cd
PeraSite <pretocki3@gmail.com>
2022-09-19 14:34:40 +0900

    feat: InputSystem 삭제

---
+++ Assets/_GrandTheftRice/Characters/Player/Scripts/WeaponAim2DWithCursor.cs
@@ -0,0 +1,34 @@
+﻿using MoreMountains.TopDownEngine;
+using UnityEngine;
+
+namespace Characters.Player.Scripts {
+	public class WeaponAim2DWithCursor : WeaponAim2D {
+		private Camera _cam;
+
+		private void Awake() {
+			_cam = Camera.main;
+		}
+
+		protected override void OnEnable() {
+			base.OnEnable();
+			// Cursor.visible = false;
+		}
+
+		protected override void HideMousePointer() {
+			if (AimControl != AimControls.Mouse) {
+				return;
+			}
+			if (GameManager.Instance.Paused) {
+				Cursor.visible = true;
+				return;
+			}
+			if (ReplaceMousePointer) {
+				var view = _cam.ScreenToViewportPoint(Input.mousePosition);
+				var isOutside = view.x is < 0 or > 1 || view.y is < 0 or > 1;
+				Cursor.visible = isOutside;
+			} else {
+				Cursor.visible = true;
+			}
+		}
+	}
+}
