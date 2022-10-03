0ba9641493fa68a5964972b8961326634eee893b
PeraSite <pretocki3@gmail.com>
2022-10-04 05:28:56 +0900

    feat: 플레이어 애니메이션 추가
    Former-commit-id: 1ea35e353e27faffbf3059243d9a455f9ab61f9d

--- Assets/_GrandTheftRice/Characters/Scripts/ReanimatorCharacter.cs
+++ Assets/_GrandTheftRice/Characters/Scripts/ReanimatorCharacter.cs
@@ -1,26 +1,33 @@
 ﻿using Aarthificial.Reanimation;
 using MoreMountains.TopDownEngine;
-using Sirenix.Utilities;
 using UnityEngine;
 
 namespace GrandTheftRice.Characters.Scripts {
 	public class ReanimatorCharacter : Character {
 		[SerializeField] private Reanimator _reanimator;
 
-		public override void AssignAnimator(bool forceAssignation = false) {
-			if (_animatorInitialized && !forceAssignation) {
-				return;
-			}
-			if (_reanimator.SafeIsUnityNull()) {
-				_reanimator = GetComponent<Reanimator>();
-			}
+		private CharacterHandleWeapon _handleWeapon;
 
-			_animatorInitialized = true;
+		protected override void OnEnable() {
+			base.OnEnable();
+			_handleWeapon = FindAbility<CharacterHandleWeapon>();
+			//TODO: Attack animation
+		}
+
+		protected override void OnHit() {
+			base.OnHit();
+			_reanimator.Set("state", Drivers.Hit);
 		}
 
 		protected override void UpdateAnimators() {
 			base.UpdateAnimators();
-			_reanimator.Set("IsMoving", _controller.CurrentMovement.magnitude > 0);
+			_reanimator.Set("isMoving", LinkedInputManager.PrimaryMovement.magnitude > 0);
+		}
+
+		private static class Drivers {
+			public const int Idle = 0;
+			public const int Hit = 1;
+			public const int Attack = 2;
 		}
 	}
 }
