5a9ac7f2d6441aeb5639dee45ab2df6cb4858a83
PeraSite <pretocki3@gmail.com>
2022-10-03 16:36:18 +0900

    chore: 폴더 및 파일 정리

--- Assets/_GrandTheftRice/Characters/Scripts/Data/CharacterStat.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-namespace GrandTheftRice {
-	public class CharacterStat : ScriptableObject {
-		[field: SerializeField] public float MaxHealth { get; private set; }
-		[field: SerializeField] public float MoveSpeed { get; private set; }
-	}
-}
