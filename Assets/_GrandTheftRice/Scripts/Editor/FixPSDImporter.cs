d420d1a44a39127df518a6df8cc5d0dd77604b31
PeraSite <pretocki3@gmail.com>
2022-10-04 14:59:25 +0900

    feat: PSB 임포트 수정
    Former-commit-id: c8855b6ae1c37eaf62156c5cf0b24d38d3249f03

---
+++ Assets/_GrandTheftRice/Scripts/Editor/FixPSDImporter.cs
@@ -0,0 +1,6 @@
+﻿public static class FixPSDImporter {
+	[UnityEditor.InitializeOnLoadMethod]
+	public static void ResetPSDImporterFoldout() {
+		UnityEditor.EditorPrefs.DeleteKey("PSDImporterEditor.m_PlatformSettingsFoldout");
+	}
+}
