dd7d91a3d6df6dd5f9bee6702f7ff00c2367358b
PeraSite <pretocki3@gmail.com>
2022-10-04 22:49:29 +0900

    feat: 기초 엔딩 시스템 추가
    Former-commit-id: 066f509ab2fdc13f034d129eaa26da7ebc30a638

---
+++ Assets/_GrandTheftRice/Scripts/Runtime/System/EndingSystem.cs
@@ -0,0 +1,37 @@
+﻿using MoreMountains.Tools;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace GrandTheftRice.System {
+	public class EndingSystem : MMSingleton<EndingSystem> {
+		[Title("설정")]
+		[Tooltip("게임 전체 플레이 타임(초)")]
+		[field: SerializeField] public int GameTime { get; private set; } = 600;
+
+		[ReadOnly]
+		[Tooltip("남은 플레이 타임(초)")]
+		public int CurrentTime;
+
+		private float _timer;
+
+		protected override void Awake() {
+			base.Awake();
+			CurrentTime = 0;
+		}
+
+		private void Update() {
+			_timer += Time.deltaTime;
+			if (_timer >= 1) {
+				_timer = 0;
+				CurrentTime++;
+				if (CurrentTime >= GameTime) {
+					EndGame();
+				}
+			}
+		}
+
+		private void EndGame() {
+			Debug.Log("game end!");
+		}
+	}
+}
--- Assets/_GrandTheftRice/Scripts/Runtime/UI/TimerUI.cs
+++ Assets/_GrandTheftRice/Scripts/Runtime/UI/TimerUI.cs
@@ -1,21 +1,17 @@
-﻿using System;
-using Sirenix.OdinInspector;
+﻿using GrandTheftRice.System;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace GrandTheftRice.UI {
 	public class TimerUI : MonoBehaviour {
 		//TODO: 실제 시간을 받아와서 표시해야함
-		public int MaxTime = 60;
-		public int CurrentTime = 30;
-
 		[SerializeField] private Image _bar;
 		[SerializeField] private Image _handle;
 
 		[SerializeField] private float _handleMinX;
 		[SerializeField] private float _handleMaxX;
 
-		public float Percent => (float) CurrentTime / MaxTime;
+		public float Percent => (float) EndingSystem.Instance.CurrentTime / EndingSystem.Instance.GameTime;
 
 		private void Update() {
 			_bar.fillAmount = Percent;
