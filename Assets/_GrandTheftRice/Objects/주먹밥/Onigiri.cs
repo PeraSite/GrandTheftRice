57011e53f0a5780ba08696edf55d4f2981c3255a
PeraSite <pretocki3@gmail.com>
2022-10-04 19:24:02 +0900

    feat: 주먹밥 추가
    Former-commit-id: a3af530e59f5a7aae4f69bc156106176122fb61d

---
+++ Assets/_GrandTheftRice/Objects/주먹밥/Onigiri.cs
@@ -0,0 +1,15 @@
+﻿using MoreMountains.TopDownEngine;
+using Sirenix.Utilities;
+using UnityEngine;
+
+namespace GrandTheftRice.Objects.Onigiri {
+	public class Onigiri : Coin {
+		protected override void Pick(GameObject picker) {
+			base.Pick(picker);
+			if (!picker.TryGetComponent<Character>(out var character)) return;
+			var movement = character.FindAbility<CharacterMovement>();
+			if (movement.SafeIsUnityNull()) return;
+			movement.ApplyMovementMultiplier(1.5f, 3f);
+		}
+	}
+}
