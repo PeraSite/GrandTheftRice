c0cd6cb2eb940d612c5713ecc08863cf52fcf3a2
PeraSite <pretocki3@gmail.com>
2022-10-04 21:21:17 +0900

    feat: 주먹밥을 NPC도 먹을 수 있게 수정
    Former-commit-id: 202a552279ed015bc85c6923ea3407a84d69c220

--- Assets/_GrandTheftRice/Objects/주먹밥/Onigiri.cs
+++ Assets/_GrandTheftRice/Objects/주먹밥/Onigiri.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 namespace GrandTheftRice.Objects.Onigiri {
 	public class Onigiri : Coin {
+		protected override bool CheckIfPickable() {
+			_character = _collidingObject.GetComponent<Character>();
+			return !_character.SafeIsUnityNull();
+		}
+
 		protected override void Pick(GameObject picker) {
-			base.Pick(picker);
 			if (!picker.TryGetComponent<Character>(out var character)) return;
 			var movement = character.FindAbility<CharacterMovement>();
 			if (movement.SafeIsUnityNull()) return;
 			movement.ApplyMovementMultiplier(1.5f, 3f);
+
+			if (character.CharacterType == Character.CharacterTypes.Player) {
+				TopDownEnginePointEvent.Trigger(PointsMethods.Add, PointsToAdd);
+			}
 		}
 	}
 }
